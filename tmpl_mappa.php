7cfc4ba04038d9959c096cc9bcedc685696f7567
milesimarco <milesimarco@outlook.com>
2014-11-21 23:57:52 +0100

    added sitemap template

---
+++ tmpl_mappa.php
@@ -0,0 +1,36 @@
+<?php
+    /*
+    Template Name: Mappa del Sito
+    */
+?>
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+    <div class="post" id="post-<?php the_ID(); ?>">
+        <div class="postentry">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <h2 class="posttitle"><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+            <?php endwhile; endif; ?>
+
+                <div id="left">
+                    <h3>Pagine</h3>
+                    <ul>
+                    <?php
+                    wp_list_pages ("sort_column=menu_order&title_li=");
+                    ?>
+                    </ul>
+                </div>
+                <div id="right">
+                    <h3>Categorie</h3>
+                    <ul>
+                    <?php
+                    wp_list_categories ("hide_empty=0&sort_column=menu_order&title_li=");
+                     ?>
+                    </ul>
+                </div>
+        </div>
+    </div>
+</div>
+<?php include(TEMPLATEPATH . '/rightsidebar.php'); ?>
+<?php get_footer(); ?>
+
