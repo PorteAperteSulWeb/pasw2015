249db6e6839e09f8bd9e1bf7431084c6a6edd713
Christian <cghellere@users.noreply.github.com>
2015-01-06 08:38:04 +0100

    Aggiunto codice modulo sottopagine
    Aggiunto codice per modulo sottopagine on sidebar

--- rightsidebar.php
+++ rightsidebar.php
@@ -1,6 +1,14 @@
 <div id="rightsidebar" class="column">
 <ul>
 <?php
+if ($children && get_option( 'pasw_submenu') == '4') { ?>
+	<li class="widget widget_nav_menu">
+		<h2 class="widgettitle">Sub-Page</h2>
+	  	<ul id="subpage-sidebar" class="menu">
+	 		<?php wp_list_pages('depth=1&title_li=&child_of='.$post->ID); ?>
+		</ul>
+	</li>
+<?php }
 if ( function_exists('generated_dynamic_sidebar')) {
     if ( function_exists('dynamic_sidebar') && generated_dynamic_sidebar() ) : endif;
 } else {
