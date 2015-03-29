269d0563d7afbdd662e6eca786f80d2ad3d2b95d
Marco Milesi <milesimarco@outlook.com>
2015-03-29 23:34:45 +0200

    Update pasw2015.js

--- include/js/pasw2015.js
+++ include/js/pasw2015.js
@@ -1,16 +1,16 @@
 jQuery("document").ready(function($){
 
-	if (pasw2015_javascript_params.fixedmenu) {
+	//if (pasw2015_javascript_params.fixedmenu) {
 		var nav = $('#topbar');
 
 		$(window).scroll(function () {
-			if ( $(window).width() > 1024 && $(this).scrollTop() > pasw2015_javascript_params.headersizeh ) {
+			if ( pasw2015_javascript_params.fixedmenu && $(window).width() > 1024 && $(this).scrollTop() > pasw2015_javascript_params.headersizeh ) {
 				nav.addClass("f-nav");
 			} else {
 				nav.removeClass("f-nav");
 			}
 		});
-	}
+	//}
 
 	if (pasw2015_javascript_params.responsive) {
 		$("#responsive-controls").click(function(){
