4b82a8d588f1e474a4b8f406dfa70d15e275bb1c
Christian Ghellere <cghellere@users.noreply.github.com>
2015-06-01 11:52:23 +0200

    Update pasw2015-eulaw.php
    aggiunto pulsante nell'editor

--- include/moduli/pasw2015-eulaw.php
+++ include/moduli/pasw2015-eulaw.php
@@ -100,5 +100,31 @@ add_shortcode('cookie', 'cookie_policy');
 
 /* ========= END SHORTCODE ========== */
 
+/* ========= Buttom Editor ========== */
+function my_add_mce_button() {
+	// check user permissions
+	if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
+		return;
+	}
+	// check if WYSIWYG is enabled
+	if ( 'true' == get_user_option( 'rich_editing' ) ) {
+		add_filter( 'mce_external_plugins', 'my_add_tinymce_plugin' );
+		add_filter( 'mce_buttons', 'my_register_mce_button' );
+	}
+}
+add_action('admin_head', 'my_add_mce_button');
+
+// Declare script for new button
+function my_add_tinymce_plugin( $plugin_array ) {
+	$plugin_array['my_mce_button'] = get_template_directory_uri() . '/js/buttomeditorcookie.js';
+	return $plugin_array;
+}
+
+// Register new button in the editor
+function my_register_mce_button( $buttons ) {
+	array_push( $buttons, 'my_mce_button' );
+	return $buttons;
+}
+/* ======= END Buttom Editor ======== */
 
 }
