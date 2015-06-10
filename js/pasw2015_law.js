6409f519aeaeea7bd71550583837041a02951a80
Christian Ghellere <cghellere@users.noreply.github.com>
2015-06-10 10:24:02 +0200

    Update pasw2015_law.js
    corretto path cookie al fine di evitare di accettare ogni pagina
    inserita scadenza cookies 7 giorni

--- js/pasw2015_law.js
+++ js/pasw2015_law.js
@@ -4,5 +4,6 @@
 	var buttonText= pasw2015_law_text.button;
 	var moreText= pasw2015_law_text.more;
 	var url= pasw2015_law_text.url;
-	if ( 'yes' !== $.cookie( 'pasw_law_cookie' ) ) {$('body').prepend('<div class="pasw2015cookies" style="display: block;"><div class="cookie-pop"><p class="pasw2015cookies-banner-text">'+ bannerText +'</p> <a href="'+url+'">' + moreText + '</a>&nbsp;&nbsp;<button id="accept-cookie">' + buttonText + '</button> </div></div>');$( '#accept-cookie' ).click(function () {$.cookie( 'pasw_law_cookie', 'yes' );$( '.cookie-pop' ).remove();location.reload();});}
+	var timeexpire= 7;
+	if ( 'yes' !== $.cookie( 'pasw_law_cookie' ) ) {$('body').prepend('<div class="pasw2015cookies" style="display: block;"><div class="cookie-pop"><p class="pasw2015cookies-banner-text">'+ bannerText +'</p> <a href="'+url+'">' + moreText + '</a>&nbsp;&nbsp;<button id="accept-cookie">' + buttonText + '</button> </div></div>');$( '#accept-cookie' ).click(function () {$.cookie( 'pasw_law_cookie', 'yes', { expires: timeexpire, path: '/' } );$( '.cookie-pop' ).remove();location.reload();});}
 })(jQuery);
