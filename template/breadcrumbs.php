3c59fdd677776e35450057f227435d660c4ad4f9
Marco Milesi <milesimarco@outlook.com>
2016-02-20 11:17:05 +0100

    Update breadcrumbs.php

--- template/breadcrumbs.php
+++ template/breadcrumbs.php
@@ -1,9 +1,9 @@
+ <div class="lastmodified">
+    Ultima modifica: <?php the_modified_date('j F Y'); ?>
+</div>
 <div id="path">
 <?php
     if (!is_home() && !is_front_page()) {
-        <div class="lastmodified">
-            Ultima modifica: <?php the_modified_date('j F Y'); ?>
-        </div>
         if ( function_exists('bcn_display') ) {
             bcn_display();
         } else if ( function_exists('yoast_breadcrumb') ) {
